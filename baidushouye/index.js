e9acc5ad850befaa970e17b238311d1006bbc362
Rason <rason00@163.com>
2017-07-24 12:24:42 +0800

    Add files via upload

---
+++ baidushouye/index.js
@@ -0,0 +1,13 @@
+
+function szshow() {
+	document.getElementById("nav-2").style.display="block";
+}
+function szhide() {
+	document.getElementById("nav-2").style.display="none";
+}
+function mshow() {
+	document.getElementById("nav-3").style.display="block";
+}
+function mhide() {
+	document.getElementById("nav-3").style.display="none";
+}
