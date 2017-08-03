24c2d954b414293978d28f92bd14330b18cd3bd2
Rason <rason00@163.com>
2017-08-03 17:06:42 +0800

    添加备注
    添加代码备注说明

--- zhihuribaoapp/js/more.js
+++ zhihuribaoapp/js/more.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+// 获取缓存中的id内容
 	var key = localStorage.getItem("newId");
+// 获取缓存中的高度
 	var hKey = localStorage.getItem("wHeight");
+// 把高度赋值给另一个缓存里的key
 	localStorage.setItem("height",hKey);
+// 以id获取相关详情内容
 	moreContent(key);
 })
